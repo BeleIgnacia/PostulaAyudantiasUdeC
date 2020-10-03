d2b955b57884d74e3bd5bd79eaf0f0d3dd31e012
AmigoManuel <diazulloamanuel@gmail.com>
2020-10-02 22:17:37 -0300

    Definición de vistas sobre app navegacion

--- apps/navegacion/views.py
+++ apps/navegacion/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import TemplateView
+
+
+class Inicio(TemplateView):
+    template_name = 'navegacion/inicio.html'
+
+
+class Ayuda(TemplateView):
+    template_name = 'navegacion/ayuda.html'
+
+
+class Contacto(TemplateView):
+    template_name = 'navegacion/contacto.html'
