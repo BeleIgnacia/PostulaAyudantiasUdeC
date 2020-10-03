6ced384dd83cd5d8b3c96a005bce75f9fefab659
AmigoManuel <diazulloamanuel@gmail.com>
2020-10-02 22:48:15 -0300

    Corrección de url

--- apps/navegacion/urls.py
+++ apps/navegacion/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from apps.navegacion.views import Inicio, Ayuda, Contacto, Nosotros
 
 urlpatterns = [
-    path('inicio/', Inicio.as_view()),
+    path('', Inicio.as_view()),
     path('nosotros/', Nosotros.as_view()),
     path('ayuda/', Ayuda.as_view()),
     path('contacto/', Contacto.as_view()),
