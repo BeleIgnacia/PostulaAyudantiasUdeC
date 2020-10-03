fe50ceca738b21a942c7bd9454208b2bf3f60693
AmigoManuel <diazulloamanuel@gmail.com>
2020-10-02 21:49:12 -0300

    TODO

---
+++ apps/navegacion/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NavegacionConfig(AppConfig):
+    name = 'navegacion'
---
+++ apps/postulaciones/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostulacionesConfig(AppConfig):
+    name = 'postulaciones'
