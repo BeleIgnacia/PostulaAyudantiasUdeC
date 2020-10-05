cb84758a3ea93d9cbda7175e813de49c97fbe558
AmigoManuel <diazulloamanuel@gmail.com>
2020-10-04 23:06:41 -0300

    Definición de vistas

--- apps/plataforma/views.py
+++ apps/plataforma/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+
+class RegistrarAlumno(CreateView):
+    pass
+
+
+def iniciar_sesion(request):
+    pass
