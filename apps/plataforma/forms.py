4378d9b7b6a23703e8a39e0dde6506a6bd8e2ec5
AmigoManuel <diazulloamanuel@gmail.com>
2020-10-13 20:57:12 -0300

    Creación de usuarios de prueba para administrador, docentes y alumnos.

--- apps/plataforma/forms.py
+++ apps/plataforma/forms.py
@@ -14,22 +14,25 @@ class RegistrarAlumnoForm(UserCreationForm):
         # Fields son los campos que utilizaremos
         # Podemos añadir y quitar campos siempre que permitan ser nulos o tengan default
         fields = [
-            #'username',
             'email',
+            'first_name',
+            'last_name',
             'password1',
             'password2',
         ]
         # Labels son nombres que utilizaremos para guiarnos dentro del template
         labels = {
-            #'username': 'Nombre de usuario',
             'email': 'Dirección de correo',
+            'first_name': 'Nombre',
+            'last_name': 'Apellido',
             'password1': 'Contraseña',
             'password2': 'Repetir contraseña'
         }
         # widgets definen la entrada que se representa en el form
         widgets = {
-            #'username': forms.TextInput(attrs={'placeholder': 'Nombre de usuario'}),
             'email': forms.EmailInput(attrs={'placeholder': 'Dirección de correo'}),
+            'first_name': forms.TextInput(attrs={'placeholder': 'Nombre'}),
+            'last_name': forms.TextInput(attrs={'placeholder': 'Apellido'}),
             'password1': forms.PasswordInput(attrs={'placeholder': 'Contraseña'}),
             'password2': forms.PasswordInput(attrs={'placeholder': 'Contraseña'}),
         }
