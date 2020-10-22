65669b7b0a73871aaa5049f755d2747a75411d53
nicosaldias <nicosaldias157@gmail.com>
2020-10-22 19:03:42 -0300

    Agregado forms de las postulaciones

---
+++ apps/postulaciones/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from apps.plataforma.models import Ayudantia,Postulacion
+
+
+class RegistrarPostulacionAyudantia(forms.ModelForm):
+    class Meta:
+        model = Ayudantia
+        fields = [
+            'cursoid',
+            'semestre',
+            'descripcion',
+            'horario',
+            'requisitos',
+            'puestos',
+        ]
+        labels = {
+            'cursoid': 'Código del curso',
+            'semestre': 'Semestre',
+            'descripcion': 'Descripción breve',
+            'horario': 'Horario',
+            'requisitos': 'Requisitos del curso',
+            'puestos':'Nº de puestos vacantes',
+        }
+        widgets = {
+            'cursoid': forms.IntegerInput(attrs={'placeholder': 'Código'}),
+            'semestre': forms.TextInput(attrs={'placeholder': 'Semestre'}),
+            'descripcion': forms.TextInput(attrs={'placeholder': 'Descripción'}),
+            'horario': forms.TextInput(attrs={'placeholder': 'Horario'}),
+            'requisitos': forms.TextInput(attrs={'placeholder': 'Requisitos'}),
+            'puestos': forms.IntegerInput(attrs={'placeholder': 'Nº de puestos vacantes'}),
+        }
+
