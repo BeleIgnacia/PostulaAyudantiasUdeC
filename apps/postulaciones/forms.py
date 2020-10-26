2b0bf35e2f039be7254d14fa9a7166f271412de1
carolina <cpinto2016@udec.cl>
2020-10-26 12:15:30 -0300

    Ajustes form nueva ayudantía

--- apps/postulaciones/forms.py
+++ apps/postulaciones/forms.py
@@ -22,10 +22,10 @@ class RegistrarPostulacionAyudantia(forms.ModelForm):
             'puestos':'Nº de puestos vacantes',
         }
         widgets = {
-            'semestre': forms.NumberInput(attrs={'placeholder': 'Semestre', 'class': 'form-control'}),
+            'semestre': forms.NumberInput(attrs={'placeholder': 'Semestre', 'class': 'form-control', 'max': 3, 'min': 1}),
             'descripcion': forms.Textarea(attrs={'placeholder': 'Descripción', 'class': 'form-control'}),
             'horario': forms.TextInput(attrs={'placeholder': 'Ej: Martes 10:15 - 12:00', 'class': 'form-control'}),
             'requisito': forms.TextInput(attrs={'placeholder': 'Ej: Ingeniería de Software 2', 'class': 'form-control'}),
-            'puestos': forms.NumberInput(attrs={'placeholder': 'Nº de puestos vacantes', 'class': 'form-control'}),
+            'puestos': forms.NumberInput(attrs={'placeholder': 'Nº de puestos vacantes', 'class': 'form-control', 'max': 10, 'min': 1}),
         }
 
