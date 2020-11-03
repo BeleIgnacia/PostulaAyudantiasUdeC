852737778f5855281a3eceec5089dda7c39075cc
AmigoManuel <manuediaz@udec.cl>
2020-11-03 00:27:04 -0300

    Añadida entrada de mis ayudantias para que docente vea sus postulaciones.

--- apps/postulaciones/urls.py
+++ apps/postulaciones/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from apps.plataforma.views import Dashboard
-from apps.postulaciones.views import NuevaAyudantia, despliega_ofertas_ayudantias
+from apps.postulaciones.views import NuevaAyudantia, OfertasAyudantias, MisAyudantias
 
 urlpatterns = [
     # Docente
     path('nueva_ayudantia/', NuevaAyudantia.as_view(), name='nueva_ayudantia'),
+    path('mis_ayudantias/', MisAyudantias.as_view(), name='mis_ayudantias'),
     # Alumno 
-    path('ofertas_ayudantias/', despliega_ofertas_ayudantias.as_view(), name='listar_ofertas')
+    path('ofertas_ayudantias/', OfertasAyudantias.as_view(), name='listar_ofertas')
 ]
--- apps/postulaciones/views.py
+++ apps/postulaciones/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
-from django.http import HttpResponse , HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
 from django.views.generic import CreateView, ListView
@@ -10,23 +10,22 @@ from apps.postulaciones.forms import RegistrarPostulacionAyudantia
 from apps.plataforma.models import Curso, Usuario
 from apps.postulaciones.models import Ayudantia, Postulacion
 from django import forms
-# Create your views here.
 
 
-class  despliega_ofertas_ayudantias(LoginRequiredMixin, ListView):
-   login_url = '/iniciar/'
-   model = Ayudantia
-   template_name= 'plataforma/desplegar_ofertas_ayudantias.html'
+class OfertasAyudantias(LoginRequiredMixin, ListView):
+    login_url = '/iniciar/'
+    model = Ayudantia
+    template_name = 'plataforma/desplegar_ofertas_ayudantias.html'
 
 
 class NuevaAyudantia(UserPassesTestMixin, CreateView):
-    login_url = '/iniciar/' #Redirecciona en caso de no haber iniciado sesión
+    login_url = '/iniciar/'  # Redirecciona en caso de no haber iniciado sesión
     model = Ayudantia
     form_class = RegistrarPostulacionAyudantia
     template_name = 'postulaciones/nueva_ayudantia.html'
     success_url = reverse_lazy('postulaciones:nueva_ayudantia')
 
-    #Comprueba que el usuario accediendo es docente
+    # Comprueba que el usuario accediendo es docente
     def test_func(self):
         return self.request.session.get('es_docente')
 
@@ -34,12 +33,11 @@ class NuevaAyudantia(UserPassesTestMixin, CreateView):
         context = super(NuevaAyudantia, self).get_context_data(**kwargs)
         docente = Usuario.objects.get(pk=self.request.session.get('pk_usuario', ''))
         cursos = Curso.objects.filter(docente_id=docente)
-        context['form'].fields['curso'] = forms.ModelChoiceField(queryset=cursos, 
-            empty_label="Seleccione curso", widget=forms.Select(attrs={'class':'form-control'}))
+        context['form'].fields['curso'] = forms.ModelChoiceField(queryset=cursos, empty_label="Seleccione curso", widget=forms.Select(attrs={'class': 'form-control'}))
         if 'form' not in context:
             context['form'] = self.form_class(self.request.GET)
         return context
-    
+
     def form_valid(self, form):
         instance = form.save(commit=False)
 
@@ -48,7 +46,16 @@ class NuevaAyudantia(UserPassesTestMixin, CreateView):
         for curso in cursos:
             if instance.curso.codigo == curso.codigo:
                 messages.error(self.request, "Ya existe una ayudantía para este curso.")
-                return HttpResponseRedirect(self.request.path_info) 
+                return HttpResponseRedirect(self.request.path_info)
 
         instance.save()
         return HttpResponseRedirect(reverse_lazy('postulaciones:nueva_ayudantia'))
+
+
+class MisAyudantias(ListView):
+    model = Ayudantia
+    template_name = 'postulaciones/mis_ayudantias.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MisAyudantias, self).get_context_data(**kwargs)
+        return context
