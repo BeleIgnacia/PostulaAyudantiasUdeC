0c1c7de2398da651de43ff1a5dbe025e03f4d083
AmigoManuel <diazulloamanuel@gmail.com>
2020-11-06 00:28:14 -0300

    Implementación de edición de perfil.

--- apps/plataforma/models.py
+++ apps/plataforma/models.py
@@ -10,6 +10,23 @@ class Usuario(User):
     # Identificadores para tipo de usuario
     es_administrador = models.BooleanField(default=False)
     es_docente = models.BooleanField(default=False)
+    # Información adicional
+    intereses = models.TextField()
+    ETAPAS = (
+        (0, 'Primer año'),
+        (1, 'Segundo año'),
+        (2, 'Tercer año'),
+        (3, 'Cuarto año'),
+        (4, 'Quinto año'),
+        (5, 'Sexto año o superior')
+    )
+    AREAS = (
+        ('0', 'Pregrado'),
+        ('1', 'Postgrado'),
+        ('2', 'Externo')
+    )
+    etapa_carrera = models.IntegerField(default=0, choices=ETAPAS)
+    area = models.CharField(max_length=20, default=0, choices=AREAS)
 
     # Esta función se usa para representar el objeto al realizar una query
     def __str__(self):
--- apps/plataforma/urls.py
+++ apps/plataforma/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from apps.plataforma.views import Dashboard, RegistrarDocente, NuevoCurso
+from apps.plataforma.views import Dashboard, RegistrarDocente, NuevoCurso, ActualizarPerfil
+
 urlpatterns = [
     path('', Dashboard.as_view(), name='dashboard'),
+    path('perfil/<pk>/', ActualizarPerfil.as_view(), name='perfil'),
     # Administrador
     path('nuevo_docente/', RegistrarDocente.as_view(), name='registrar_docente'),
     # Docente
     path('nuevo_curso/', NuevoCurso.as_view(), name='nuevo_curso')
     # Alumno
-    
 ]
