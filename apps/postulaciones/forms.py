7e217658f355ea01b0dbaadbe32f7d701ff7ba92
AmigoManuel <diazulloamanuel@gmail.com>
2020-11-14 18:20:06 -0300

    Integración nueva ayudantía

--- apps/postulaciones/forms.py
+++ apps/postulaciones/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from apps.postulaciones.models import Ayudantia,Postulacion
+from apps.postulaciones.models import Ayudantia, Postulacion
 from apps.postulaciones.models import Usuario
+
 
 class RegistrarPostulacionAyudantia(forms.ModelForm):
     class Meta:
@@ -19,11 +20,11 @@ class RegistrarPostulacionAyudantia(forms.ModelForm):
             'descripcion': 'Descripción breve',
             'horario': 'Horario',
             'requisito': 'Requisitos del curso',
-            'puestos':'Nº de puestos vacantes',
+            'puestos': 'Nº de puestos vacantes',
         }
         widgets = {
             'semestre': forms.NumberInput(attrs={'placeholder': 'Semestre', 'class': 'form-control', 'max': 3, 'min': 1}),
-            'descripcion': forms.Textarea(attrs={'placeholder': 'Descripción', 'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'placeholder': 'Descripción', 'class': 'form-control', 'cols': 6, 'rows': 6}),
             'horario': forms.TextInput(attrs={'placeholder': 'Ej: Martes 10:15 - 12:00', 'class': 'form-control'}),
             'requisito': forms.TextInput(attrs={'placeholder': 'Ej: Ingeniería de Software 2', 'class': 'form-control'}),
             'puestos': forms.NumberInput(attrs={'placeholder': 'Nº de puestos vacantes', 'class': 'form-control', 'max': 10, 'min': 1}),
@@ -35,13 +36,13 @@ class RegistrarPostulacionAlumno(forms.ModelForm):
         model = Postulacion
         fields = [
             'semestreramo',
-            'nota',          
+            'nota',
         ]
         labels = {
-            'semestreramo':'Semestre de rendición',
-            'nota':'Nota',
+            'semestreramo': 'Semestre de rendición',
+            'nota': 'Nota',
         }
         widgets = {
-            'semestreramo':forms.TextInput(attrs={'placeholder': 'Ej: 2019-2'}),
-            'nota':forms.TextInput(attrs={'placeholder': 'Promedio con el que aprobó'}),
-        }   
+            'semestreramo': forms.TextInput(attrs={'placeholder': 'Ej: 2019-2'}),
+            'nota': forms.TextInput(attrs={'placeholder': 'Promedio con el que aprobó'}),
+        }
