f0bd485554a81ac3e8d4f0844ec9b1226428870d
Roberto <Robertoavi2016@inf.udec.cl>
2020-12-13 01:38:04 -0300

    filtrar postulaciones por curso

--- apps/postulaciones/urls.py
+++ apps/postulaciones/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
 from apps.plataforma.views import Dashboard
-from apps.postulaciones.views import NuevaAyudantia, OfertasAyudantias, PostulacionesRealizadas, PostulacionesAlumno
+from apps.postulaciones.views import NuevaAyudantia, OfertasAyudantias, PostulacionesRealizadas, PostulacionesAlumno, MisCursos
 
 urlpatterns = [
     # Docente
     path('nueva_ayudantia/', NuevaAyudantia.as_view(), name='nueva_ayudantia'),
     path('mis_ayudantias/', PostulacionesRealizadas.as_view(), name='mis_ayudantias'),
+    #path('mis_ayudantias/', MisCursos.as_view(), name='mis_cursos'),
     # Alumno 
     path('mis_postulaciones/', PostulacionesAlumno.as_view(), name='mis_postulaciones'),
     path('ofertas_ayudantias/', OfertasAyudantias.as_view(), name='listar_ofertas')
